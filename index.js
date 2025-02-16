8e71263384e9fdeab09d59a05d6dc41232bcbcec
Vivekgpta <131643827+Vivekgpta@users.noreply.github.com>
2025-02-16 13:31:57 +0530

    login details check

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 let login_email = "shubh@gmail.com";
 
 let login_pass = "123456";
+
+let input_email = "shubh@gmail.com";
+
+let input_pass = "123456";
+
+
+if(login_email == input_email && login_pass == input_pass){
+console.log("login successfull");
+} 
+else {
+console.log("wrong password")'
+
+}
